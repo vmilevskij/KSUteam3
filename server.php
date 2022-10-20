f0fa77ea0e26d5f8006f093783c5a49fde6ce013
vmilevskij <114560566+vmilevskij@users.noreply.github.com>
2022-10-20 12:21:26 +0300

    Revert "Revert "Merge branch 'Testing' of https://github.com/vmilevskij/KSUteam3 into Testing""
    This reverts commit d7452b8a4d36e893ca1b895c129378d0057780d2.

---
+++ server.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+$uppercase    = "";
+$lowercase    = "";
+$number       = "";
+$specialchars = "";
+$letters = "";
+$numbers = "";
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'news-website');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+  // Validate password strength
+$uppercase    = preg_match('@[A-Z]@', $password_1);
+$lowercase    = preg_match('@[a-z]@', $password_1);
+$number       = preg_match('@[0-9]@', $password_1);
+$specialchars = preg_match('@[^\w]@', $password_1);
+
+$letters = preg_match('@[a-z]@', $username);
+$numbers       = preg_match('@[0-9]@', $username);
+
+if (!$uppercase || !$lowercase || !$number || !$specialchars || strlen($password_1) < 8) {
+  array_push($errors, "The password must be at least 8 characters in length, at least one upper case, at least one number and least one special character.");
+  
+} 
+
+
+
+if (!$letters|| !$numbers|| !$number || strlen($username) < 2) {
+  array_push($errors, "The username must be at least 2 characters in length, and at least one number");
+} 
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+  
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['username'] = $username;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: index.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+  }
+  
+
+
+
+
+
+
+  ?>
